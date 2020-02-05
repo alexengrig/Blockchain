bc232cd8d98698b854ad9daa2c835384f7d7876d
Grig Alex <alexengrigdev@gmail.com>
2020-02-06 00:10:02 +0300

    Refactor Block

--- Blockchain/task/src/blockchain/block/Block.java
+++ Blockchain/task/src/blockchain/block/Block.java
@@ -1,13 +1,17 @@
-package blockchain;
+package blockchain.block;
 
 public interface Block {
-    int getId();
+    long getId();
+
+    long getRuntime();
 
     long getTimestamp();
 
     long getMagicNumber();
 
+    String getHash();
+
     String getPreviousHash();
 
-    String getHash();
+    String getOwner();
 }
