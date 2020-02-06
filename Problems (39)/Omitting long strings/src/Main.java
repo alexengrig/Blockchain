a832be0e81c2402c9873792e870a7dc469919a60
Grig Alex <alexengrigdev@gmail.com>
2020-02-06 22:31:41 +0300

    Add "Omitting long strings" problem solution

---
+++ Problems (39)/Omitting long strings/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static List<String> omitLongStrings(List<String> strings) {
+        return strings.stream().map(String::trim).filter(s -> s.length() < 4).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        List<String> list = new ArrayList<>(Arrays.asList(str.split(" ")));
+        omitLongStrings(list).forEach(System.out::println);
+    }
+}
