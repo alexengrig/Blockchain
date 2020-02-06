86da493292f846b49021cbe1077123c6147f5862
Grig Alex <alexengrigdev@gmail.com>
2020-02-06 22:35:18 +0300

    Add "Numbers filtering" problem solution

---
+++ Problems (40)/Numbers filtering/src/Main.java
@@ -0,0 +1,6 @@
+public static IntStream createFilteringStream(IntStream evenStream, IntStream oddStream) {
+    return IntStream.concat(evenStream, oddStream)
+            .filter(i -> i % 3 == 0 && i % 5 == 0)
+            .sorted()
+            .skip(2);
+}
