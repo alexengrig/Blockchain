19c2e5eb45b7f196b5f7e6e41e5f33400e7ebbba
Grig Alex <alexengrigdev@gmail.com>
2020-02-09 01:30:51 +0300

    Refactor NZeros#getNextStatus

--- Blockchain/task/src/blockchain/NZeros.java
+++ Blockchain/task/src/blockchain/NZeros.java
@@ -21,18 +21,18 @@ public class NZeros {
 
     public String getNextStatus() {
         String status = "N stays the same";
-        int variant = random.nextInt();
-        if (variant % 4 == 0) {
-            increasedCount = 0;
-            decreasedCount = 0;
-        } else if (variant % 2 == 0) {
+        int variant = random.nextInt(4);
+        if (variant == 0 || variant == 1) {
             ++count;
             status = "N was increased to " + ++increasedCount;
             decreasedCount = 0;
-        } else if (count > 0) {
+        } else if (count > 0 && variant == 2) {
             --count;
             status = "N was decreased by " + ++decreasedCount;
             increasedCount = 0;
+        } else {
+            increasedCount = 0;
+            decreasedCount = 0;
         }
         return status;
     }
